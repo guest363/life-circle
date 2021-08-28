cf19963c7caaf6f4ce253d4536a9195e70c23f7b
Oleg Moiseev <guest363@gmail.com>
2021-08-28 19:14:56 +0300

    Update depends

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,5 +1,7 @@
 <script>
+  import { blur } from "svelte/transition";
   import { LIFE_PARTS } from "./constants";
+  import { quintOut } from "svelte/easing";
   import { todo_display } from "./store/todo-display";
 </script>
 
@@ -9,7 +11,7 @@
   >
 
   {#if $todo_display}
-    <div class="todo">
+    <div class="todo" in:blur="{{ amount: 10 }}" out:blur="{{ amount: 10 }}">
       {#each Object.values(LIFE_PARTS) as part}
         <h2>{part}</h2>{/each}
     </div>
